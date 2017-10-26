9179546b30813781f62975fc1b8aa5708c3213a3
Andrew Jewett <jewett.aij@gmail.com>
2017-10-26 02:58:37 -0700

    improvements to the minimization protocol in the example ("run.in.min")

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
 
   url='https://github.com/jewettaij/cellpack2moltemplate',
 
-  download_url='https://github.com/jewettaij/cellpack2moltemplate/archive/v0.0.3.zip',
+  download_url='https://github.com/jewettaij/cellpack2moltemplate/archive/v0.0.4.zip',
 
-  version='0.0.3',
+  version='0.0.4',
 
   keywords=['CellPACK', 'moltemplate', 'simulation', 'LAMMPS'],
 
